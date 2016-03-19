7a8cd761705d2357610e2357ac643881baac9c56
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-03-19 17:20:28 +0000

    Added print of class_average and letter_grade

--- school_class_manager.py
+++ school_class_manager.py
@@ -55,3 +55,6 @@ def get_class_average(students):
     for student in students:
         results.append(get_average(student))
     return average(results)
+
+print get_class_average(students)
+print get_letter_grade(get_average(students))
